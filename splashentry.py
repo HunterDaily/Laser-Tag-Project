18e8eaae3100bd58efa829a115e0681bcfb8ef17
ParkerFunnyman <140291094+ParkerFunnyman@users.noreply.github.com>
2026-09-16 14:55:34 -0500

    Fixed path to logo.jpg

--- splashentry.py
+++ splashentry.py
@@ -21,7 +21,7 @@ gameScreen = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("Entry Terminal")
 
 #Splash screen 
-logo = pygame.image.load("photon-main\logo.jpg")
+logo = pygame.image.load("photon-main/logo.jpg")
 logo = pygame.transform.scale(logo, (800, 400))
 
 
